5d08f34cf64db564c058a46c845cbda68a5e31d2
夜雨飘零 <yeyupiaoling@foxmail.com>
2024-01-09 09:49:27 +0800

    增加SphereFace2损失函数

--- mvector/trainer.py
+++ mvector/trainer.py
@@ -28,7 +28,7 @@ from mvector.models.campplus import CAMPPlus
 from mvector.models.ecapa_tdnn import EcapaTdnn
 from mvector.models.eres2net import ERes2Net
 from mvector.models.fc import SpeakerIdentification
-from mvector.models.loss import AAMLoss, CELoss, AMLoss, ARMLoss, SubCenterLoss
+from mvector.models.loss import AAMLoss, CELoss, AMLoss, ARMLoss, SubCenterLoss, SphereFace2
 from mvector.models.res2net import Res2Net
 from mvector.models.resnet_se import ResNetSE
 from mvector.models.tdnn import TDNN
@@ -172,6 +172,8 @@ class MVectorTrainer(object):
             loss_args = loss_args if loss_args is not None else {}
             if use_loss == 'AAMLoss':
                 self.loss = AAMLoss(**loss_args)
+            elif use_loss == 'SphereFace2':
+                self.loss = SphereFace2(**loss_args)
             elif use_loss == 'SubCenterLoss':
                 self.loss = SubCenterLoss(**loss_args)
             elif use_loss == 'AMLoss':
